d4e71f8d307d4f2a33c84e3795ff23ae6b345b21
louisepujante <louisegwenpujante@gmail.com>
2022-04-21 14:14:36 +0800

    changed datepicker trigger to text field

--- lib/views/widgets/date_field.dart
+++ lib/views/widgets/date_field.dart
@@ -18,6 +18,32 @@ class DateField extends StatefulWidget {
 class _DateFieldState extends State<DateField> {
   DateTime selectedDate = DateTime.now();
 
+  getDateInput() async {
+    DateTime? newDate = await showDatePicker(
+      context: context,
+      initialDate: selectedDate,
+      firstDate: widget.firstDate,
+      lastDate: widget.lastDate,
+      builder: (context, child) {
+        return Theme(
+          data: ThemeData(
+            colorScheme: const ColorScheme.light(
+              primary: AppColor.secondary,
+            ),
+          ),
+          child: child!,
+        );
+      },
+    );
+    if (newDate != null) {
+      setState(
+        () {
+          selectedDate = newDate;
+        },
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -29,40 +55,13 @@ class _DateFieldState extends State<DateField> {
         ),
         TextFormField(
           readOnly: true,
-          key: Key(selectedDate.toString()),
+          key: UniqueKey(),
+          onTap: getDateInput,
           initialValue: DateFormat('EEE, d MMM yyyy').format(selectedDate),
-          decoration: InputDecoration(
-            suffixIcon: IconButton(
-              splashRadius: 20,
-              icon: const Icon(
-                Icons.event,
-                color: AppColor.gray,
-              ),
-              onPressed: () async {
-                DateTime? newDate = await showDatePicker(
-                  context: context,
-                  initialDate: DateTime.now(),
-                  firstDate: widget.firstDate,
-                  lastDate: widget.lastDate,
-                  builder: (context, child) {
-                    return Theme(
-                      child: child!,
-                      data: ThemeData(
-                        colorScheme: ColorScheme.light(
-                          primary: AppColor.secondary,
-                        ),
-                      ),
-                    );
-                  },
-                );
-                if (newDate != null) {
-                  setState(
-                    () {
-                      selectedDate = newDate;
-                    },
-                  );
-                }
-              },
+          decoration: const InputDecoration(
+            suffixIcon: Icon(
+              Icons.event,
+              color: AppColor.gray,
             ),
           ),
         ),
