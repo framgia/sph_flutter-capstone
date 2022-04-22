287dc20e142f299d8856be5f99b1d04e2b9aa188
Erron Intila <88467532+erronerron@users.noreply.github.com>
2022-04-22 09:03:44 +0800

    Setup SQLite database (#8)
    * Setup sqlite database
    
    * Format code

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:intl/intl.dart';
+import 'package:sun_flutter_capstone/models/model.dart';
 // import 'package:auto_route/auto_route.dart'; Uncomment to use in the future
 import 'package:sun_flutter_capstone/utils/routes/router.gr.dart';
 import 'package:sun_flutter_capstone/consts/global_style.dart';
 
-import 'package:sun_flutter_capstone/consts/global_style.dart';
-
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
     statusBarColor: Colors.transparent,
   ));
+
+  final bool isInitialized = await ExpenseDBModel().initializeDB();
+  if (isInitialized) runSamples();
+
   runApp(ProviderScope(
     child: MyApp(),
   ));
@@ -34,4 +39,71 @@ class MyApp extends StatelessWidget {
       routeInformationParser: _appRouter.defaultRouteParser(),
     );
   }
+}
+
+Future<bool> runSamples() async {
+  // add sample categories
+  await addSampleCategories();
+
+  // add sample expenses
+  await addSampleExpenses();
+
+  return true;
+}
+
+Future<void> addSampleCategories() async {
+  final category = await Category().select().toSingle();
+  if (category == null) {
+    await Category(
+      name: 'Food',
+      createdAt: DateTime.now(),
+      updatedAt: DateTime.now(),
+    ).save();
+    await Category(
+      name: 'Electricity',
+      createdAt: DateTime.now(),
+      updatedAt: DateTime.now(),
+    ).save();
+    await Category(
+      name: 'Water',
+      createdAt: DateTime.now(),
+      updatedAt: DateTime.now(),
+    ).save();
+  }
+  return;
+}
+
+Future<void> addSampleExpenses() async {
+  final expense = await Expense().select().toSingle();
+  final category = await Category().select().toSingle();
+  DateTime datetime =
+      DateTime.parse(DateFormat('yyyy-MM-dd').format(DateTime.now()));
+
+  if (expense == null && category != null) {
+    await Expense(
+      description: 'Lunch',
+      amount: 100,
+      paid_at: datetime,
+      categoryId: category.id,
+      createdAt: DateTime.now(),
+      updatedAt: DateTime.now(),
+    ).save();
+    await Expense(
+      description: 'Electricity Bill',
+      amount: 1000,
+      paid_at: datetime,
+      categoryId: category.id,
+      createdAt: DateTime.now(),
+      updatedAt: DateTime.now(),
+    ).save();
+    await Expense(
+      description: 'Water bill',
+      amount: 500,
+      paid_at: datetime,
+      categoryId: category.id,
+      createdAt: DateTime.now(),
+      updatedAt: DateTime.now(),
+    ).save();
+  }
+  return;
 }
