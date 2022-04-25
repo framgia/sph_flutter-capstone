29b86b02088ccdf1ea135e8b703f60c5b76e7f4b
ErvinJMesa <ervinjeremiah.mesa@sun-asterisk.com>
2022-04-25 14:05:20 +0800

    Setup Inital UserController

--- lib/controllers/user_controller.dart
+++ lib/controllers/user_controller.dart
@@ -1,1 +1,33 @@
+import 'package:sun_flutter_capstone/models/model.dart';
 
+class UserController {
+  final userModel = Account();
+
+  Future<List<Account>> index() async {
+    return await userModel.select().toList();
+  }
+
+  void create(
+    String first_name,
+    String last_name,
+    String email,
+    String currency,
+  ) async {
+    userModel.first_name = first_name;
+    userModel.last_name = last_name;
+    userModel.email = email;
+    userModel.currency = currency;
+    userModel.createdAt = DateTime.now();
+    userModel.updatedAt = DateTime.now();
+
+    await userModel.save();
+  }
+
+  void delete(int id) {
+    userModel.select().id.equals(id).delete();
+  }
+
+  void clear() {
+    userModel.select().delete();
+  }
+}
--- lib/views/pages/transactions.dart
+++ lib/views/pages/transactions.dart
@@ -1,19 +1,57 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:sun_flutter_capstone/state/session_provider.dart';
-import 'package:sun_flutter_capstone/utils/routing.dart';
+import 'package:sun_flutter_capstone/controllers/user_controller.dart';
 import 'package:sun_flutter_capstone/views/widgets/template.dart';
 
-class TransactionsPage extends HookConsumerWidget {
+final UserController User = UserController();
+
+class TransactionsPage extends StatefulHookConsumerWidget {
   const TransactionsPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    return const Template(
+  ConsumerState<ConsumerStatefulWidget> createState() =>
+      _TransactionsPageState();
+}
+
+class _TransactionsPageState extends ConsumerState<TransactionsPage> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  Future<List<Text>> getUserList() async {
+    List<Text> list = List<Text>.empty();
+    return await User.index().then(
+      (value) => value.map<Text>(
+        (element) {
+          return Text(element.first_name!);
+        },
+      ).toList(),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Template(
       appbarTitle: Text('Transactions Page'),
       content: Center(
-        child: Text('Sample Content'),
-      ),
+          child: FutureBuilder<List<Text>>(
+        future: getUserList(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState != ConnectionState.done) {
+            return Text('Loading');
+          }
+          return Column(
+            children: snapshot.data!
+                .map<Widget>(
+                  (e) => Container(
+                    child: e,
+                  ),
+                )
+                .toList(),
+          );
+        },
+      )),
     );
   }
 }
