dbc24ea97f432886d0195d20bd7560fdf11cceed
ErickDoroy <89514595+ErickDoroy@users.noreply.github.com>
2022-04-29 13:29:12 +0800

    [Bugfix] Disable Notification Icon

--- lib/views/widgets/navigation/bottom_navbar.dart
+++ lib/views/widgets/navigation/bottom_navbar.dart
@@ -62,7 +62,7 @@ class BottomNavBar extends HookConsumerWidget {
                 NavigationButton(
                   icon: Icons.notifications_none_outlined,
                   activeIcon: Icons.notifications_active,
-                  route: Routes.notifications,
+                  route: null,
                 ),
                 NavigationButton(
                   icon: Icons.settings_outlined,
